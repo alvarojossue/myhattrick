d838a24cfeee6cf86c695fe4779a7f7b8318f6cd
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-05-13 14:23:44 -0400

    Fix styling in views. Add footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,48 @@ country_2 = Country.create(name: "Italy", continent: "Europe")
 real_madrid = country_1.teams.create(name: "Real Madrid", date_founded: Date.new(1902,3,6), city: "Madrid", stadium: "Santiago Bernabeu", fans_nickname: "Merengues")
 barcelona = country_1.teams.create(name: "Barcelona", date_founded: Date.new(1902,3,6), city: "Barcelona", stadium: "Camp Nou", fans_nickname: "Cules")
 
-# real_madrid_url = [
-# 	"http://www.marca.com/en/football/real-madrid/2016/05/12/573430f6268e3e156b8b457c.html",
-# 	"http://www.marca.com/en/football/real-madrid/2016/05/12/5734891ee2704e90648b4592.html",
-# 	"http://www.marca.com/en/football/real-madrid/2016/05/12/57344882268e3e501b8b4627.html"
-# ]
+real_madrid_url = [
+	"http://www.marca.com/en/football/real-madrid/2016/05/12/573430f6268e3e156b8b457c.html",
+	"http://www.marca.com/en/football/real-madrid/2016/05/12/5734891ee2704e90648b4592.html",
+	"http://www.marca.com/en/football/real-madrid/2016/05/12/57344882268e3e501b8b4627.html"
+]
 
-# real_madrid_url.each do |item|
-	
-# 	@the_team = real_madrid
-# 	scraper = Webscrapper.new(item)
-# 		@title = scraper.get_title
-# 		@image = scraper.get_image
-# 		@author = scraper.get_author
-# 		@date = scraper.get_date
-# 		@content = scraper.get_content
 
-# 		@the_post = @the_team.posts.new(title: @title, image_url: @image, author: @author, date: @date, content: @content, source: "Marca")
+barcelona_url = [
+	"http://www.marca.com/en/football/barcelona/2016/05/12/57348792e2704e5d548b4575.html",
+	"http://www.marca.com/en/football/barcelona/2016/05/12/573484f5e2704ec00f8b45a9.html",
+	"http://www.marca.com/en/football/barcelona/2016/05/11/57338161ca474100728b467b.html"
+]
 
-# end
+real_madrid_url.each do |item|
+
+	@the_team = real_madrid
+	scraper = Webscrapper.new(item)
+		@title = scraper.get_title
+		@image = scraper.get_image
+		@author = scraper.get_author
+		@date = scraper.get_date
+		@content = scraper.get_content
+
+		@the_post = @the_team.posts.new(title: @title, image_url: @image, author: @author, date: @date, content: @content, source: "Marca")
+		@the_post.save
+
+end
+
+barcelona_url.each do |item|
+
+	@the_team = barcelona
+	scraper = Webscrapper.new(item)
+		@title = scraper.get_title
+		@image = scraper.get_image
+		@author = scraper.get_author
+		@date = scraper.get_date
+		@content = scraper.get_content
+
+		@the_post = @the_team.posts.new(title: @title, image_url: @image, author: @author, date: @date, content: @content, source: "Marca")
+		@the_post.save
+
+end
 
 
 
